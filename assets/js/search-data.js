c0c96929ef1d3518ccaef6cc6ea871b83618c91e
Zhuochenn <49394771+Zhuochenn@users.noreply.github.com>
2026-04-25 20:44:19 +0000

    Deploying to gh-pages from @ Zhuochenn/Zhuochenn.github.io@ae4a58103c9d53494092c5c53c43b59f2dc747de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -439,7 +439,7 @@ ninja.data = [{
           section: "News",},{id: "news-native-domain-camera-lidar-calibration-has-been-accepted-by-r-al-congrats-to-ni",
           title: 'Native-Domain Camera–LiDAR Calibration has been accepted by R-AL! Congrats to Ni!',
           description: "",
-          section: "News",},{id: "news-i-am-invited-by-springer-nature-as-lead-guest-editor-to-publish-a-special-issue-regarding-visual-tactile-sensing-in-the-international-journal-of-computer-vision-ijcv-ccf-a-stay-tuned",
+          section: "News",},{id: "news-i-am-invited-by-springer-nature-as-lead-guest-editor-to-publish-a-special-issue-regarding-visual-tactile-sensing-in-the-international-journal-of-computer-vision-ijcv-stay-tuned",
           title: 'I am invited by Springer Nature as lead guest editor to publish a...',
           description: "",
           section: "News",},{
